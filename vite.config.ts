cb5fcb541e17bc36886e802f8eeb34508fab0724
simoneprado83 <si.prado83@gmail.com>
2025-08-20 20:12:43 -0300

    Corrige base para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/prapets-site-react-ts/', // Adicione esta linha
   plugins: [react()],
 })
